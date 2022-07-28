c7cbdf22a99224de8b8c6af934a17955dce0df16
RevnSukaTuru <109857886+RevnSukaTuru@users.noreply.github.com>
2022-07-28 15:17:15 +0700

    Update infojarot.js

--- plugins/infojarot.js
+++ plugins/infojarot.js
@@ -2,25 +2,25 @@ let handler  = async (m, { conn, usedPrefix }) => {
 conn.reply(m.chat, `
 ╭─˗ˏˋ *INFORMASI OWNER* ´ˎ˗
 │ ✎ _Nama_ : Jarot
-│ ✎ _Sekolah_ : Lulus
-│ ✎ _Umur_ : 17
-│ ✎ _Asal_ : Jawa timur
-│ ✎ _Status_ : Turu deck
+│ ✎ _Sekolah_ : Bocel Esempe
+│ ✎ _Umur_ : Bocel
+│ ✎ _Asal_ : Aceh
+│ ✎ _Status_ : Turu
 │ ✎ _Official Grup 1_ :
-│    https://chat.whatsapp.com/IqZke0cAG6G3iVsnB9myfL
+│    https://chat.whatsapp.com/H6AeFd5BKNcIa6uErFxoTZ
 │ ✎ _Official Grup 2_ :
-│    https://chat.whatsapp.com/HjRHck1G3WRHOx97fJkdMN
+│    https://chat.whatsapp.com/IcdjJs5yzQH0TwFwEuSoS8
 │ ✎ _Youtube _:
-│    https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ
+│    https://youtube.com/channel/VanzzXYT
 │ ✎ _Instagram_ : 
-│    https://instagram.com/oscarbotz_
+│    https://instagram.com/vanzzxyt_official
 │ ✎ _WhatsApp_ :
-│    wa.me/6285850539404
+│    wa.me/6285760165634
 ╰───────────────────
 `.trim(), m)
 }
 
-handler.help = ['infojarot']
+handler.help = ['inforevan]
 handler.tags = ['main', 'utama']
 handler.command = /^(infojarot)$/i
 
